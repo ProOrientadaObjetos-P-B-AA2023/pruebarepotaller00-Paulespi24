f03871d85e9bd380760d3701f92f3e2aad0ebfbf
Paulespi24 <paulespi2404@gmail.com>
2023-04-12 17:02:24 -0500

    demo2

---
+++ ProyectoPaulEspinosa/src/proyectopaulespinosa/ProyectoPaulEspinosa.java
@@ -0,0 +1,11 @@
+
+package proyectopaulespinosa;
+
+public class ProyectoPaulEspinosa {
+
+    public static void main(String[] args) {
+        System.out.println("Loja, una ciudad llena de arte y naturaleza con 400322");
+
+    }
+    
+}
